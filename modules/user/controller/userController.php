a3c8992c4d73a69bef17f50015c4f8635b13d522
Juan Francisco <fherrera@softin.mx>
2014-11-09 03:40:50 -0600

    static assests

---
+++ modules/user/controller/userController.php
@@ -0,0 +1,16 @@
+<?php
+	require_once('modules/user/model/user.php');
+	class userController{
+		var $user = NULL;
+		
+		public function __construct(){
+			$this->user = new user();
+		}
+
+		//Show()
+		public function home($view){
+			$this->user->show($view);
+		}
+
+	}
+?>
